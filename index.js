77d28e1e64f87c97dd3f8e582037433c3474216c
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-15 19:18:04 +0100

    Added basic backend for heroku

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const { port, dbURI } = require('./config/environment');
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbURI);
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => console.log(`Express running on port ${port}.`));
+
+module.exports = app;
