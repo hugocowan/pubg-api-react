d4d12c99dce4838e2eb4a37c47a7da3fd904c3ad
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-15 16:39:25 +0100

    Successfully stored telemetry links for all games of current season.

--- src/app.js
+++ src/app.js
@@ -17,37 +17,53 @@ class App extends React.Component{
     })
       .then(res => {
         const telemetry = res.data.data[0].relationships.matches.data;
-        this.setState({telemetry, message: 'Request Sent.'}, () => {
+        this.setState({ telemetry, message: 'Request Sent.' }, () => {
           this.state.telemetry.forEach((match, index) => {
             const matchNumber = `${index+1}`;
+            // console.log(matchNumber);
             axios.get(`https://api.playbattlegrounds.com/shards/pc-eu/matches/${match.id}`, {
               headers: {
                 Accept: 'application/vnd.api+json'
               }
             })
               .then(res => {
-                console.log(res.data, res.data.data.relationships.assets.data[0].id);
-                const id = res.data.data.relationships.assets.data[0].id;
-                this.setState({
-                  matches: {
-                    [matchNumber]: {
-                      assetId: id
-                    }
-                  }
+                // console.log(res.data, res.data.data.relationships.assets.data[0].id);
+                const matches = { ...this.state.matches };
+                let id;
+                // console.log('hi, ',matches);
+
+                res.data.included.forEach((asset) => {
+                  if(asset.id === res.data.data.relationships.assets.data[0].id)
+                    id = asset.attributes.URL;
+                });
+
+                matches[matchNumber] = {
+                  id,
+                  attributes: res.data.data.attributes
+                };
+
+                const matchesArray = Object.keys(matches).map(key => {
+                  return matches[key];
+                });
+
+                this.setState({ matches: matchesArray, message: 'Request Received.' }, () => {
+                  console.log(this.state);
                 });
               });
           });
         });
-
       });
   }
 
+  getMatch = () => {
+    axios.get();
+  }
+
   render(){
-    if(!this.state.telemetry) return this.state.message;
-    // telemetry
+    if(!this.state.matches) return this.state.message;
     return(
       <div>
-        {this.state.telemetry.map(match =>
+        {this.state.matches.map(match =>
           <div key={match.id}>
             <p>Match ID: {match.id}</p>
           </div>
