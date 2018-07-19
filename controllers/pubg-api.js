507292239c44464017024ab90e8af5430a2321c0
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-07-19 14:00:12 +0100

    Server will get data from DB if stored data is <1min old. This prevents 3rd party get request spamming and reduces likelihood of reaching the 10 requests/min limit.

--- controllers/pubg-api.js
+++ controllers/pubg-api.js
@@ -2,102 +2,122 @@ const rp = require('request-promise');
 const Season = require('../models/season');
 
 function playerSeason(req, res, next) {
-  console.log('Getting season...');
+  console.log('Checking season DB...');
+
+  let oldSeason;
   const tempMatch = [];
 
-  function saveData(seasonData) {
-    // console.log('tempMatch: ',tempMatch);
+  // console.log(req.params.username);
 
-    Season
-      .find()
-      .byName(req.params.username)
-      .then(season => {
-        if(season[0]) season[0].remove();
-      })
-      .then(() =>{
-        Season
-          .create(seasonData)
-          .then(season => res.json(season))
-          .catch(next);
-      });
+
+  Season
+    .find()
+    .byName(req.params.username)
+    .then(season => {
+      if(!season[0]) {
+        console.log('No old season available. Getting new season...');
+        getNewSeason();
+      } else {
+
+        const seasonDate = new Date(season[0].attributes.createdAt).getTime();
+        const currentDate = new Date().getTime();
+        oldSeason = season[0];
+        const timer = (seasonDate + 60000 - currentDate)/1000;
+
+        if(timer < 0){
+          console.log('Timer\'s up. Getting new season...');
+          season[0].remove();
+          getNewSeason();
+
+        } else {
+          console.log(`${timer} seconds remaining. Showing old season...`);
+          showOldSeason(season[0]);
+        }
+      }
+
+    })
+    .catch(next);
+
+
+  function showOldSeason(oldSeason) {
+    console.log('Season sent.');
+    res.json(oldSeason);
   }
 
-  function getData() {
+  function showNewSeason(seasonData) {
     Season
-      .find()
-      .byName(req.params.username)
-      .then(season => {
-        if(!season[0]) throw 'Too many requests.';
-        res.json(season[0]);
-      })
+      .create(seasonData)
+      .then(season => res.json(season))
       .catch(next);
   }
 
-  return rp({
-    method: 'GET',
-    url: `https://api.playbattlegrounds.com/shards/pc-eu/players?filter[playerNames]=${req.params.username}`,
-    headers: {
-      Authorization: `Bearer ${process.env.PUBG_API_KEY}`,
-      Accept: 'application/vnd.api+json'
-    },
-    json: true
-  })
-    .then(season => {
-      console.log('season received.');
-      const seasonData = season.data[0];
-      seasonData.name = season.data[0].attributes.name;
-      const matches = seasonData.relationships.matches.data;
-
-      matches.forEach(match => {
-        rp({
-          method: 'GET',
-          url: `https://api.playbattlegrounds.com/shards/pc-eu/matches/${match.id}`,
-          headers: {
-            Accept: 'application/vnd.api+json'
-          },
-          json: true
-        })
-          .then(res => {
-            const id = res.data.id;
-            const attrs = res.data.attributes;
-            // console.log(res.data.attributes.createdAt);
-            const telemetryId = res.data.relationships.assets.data[0].id;
-            const maps = {
-              Erangel_Main: 'Erangel',
-              Desert_Main: 'Miramar',
-              Savage_Main: 'Sanhok'
-            };
-            let telemetryURL;
-            res.included.forEach((asset) => {
-              if(asset.id === telemetryId){
-                telemetryURL = asset.attributes.URL;
-                tempMatch.push({
-                  id,
-                  telemetryURL,
-                  createdAt: attrs.createdAt,
-                  duration: attrs.duration,
-                  gameMode: attrs.gameMode,
-                  // isCustomMatch: attrs.isCustomMatch,
-                  mapName: maps[attrs.mapName],
-                  // stats: attrs.stats,
-                  // tags: attrs.tags,
-                  // titleId: attrs.titleId,
-                  shardId: attrs.shardId
-                });
-              }
-            });
-            if(tempMatch.length === matches.length){
-              seasonData.matches = tempMatch;
-              saveData(seasonData);
-            }
-          });
-      });
+  function getNewSeason() {
+    rp({
+      method: 'GET',
+      url: `https://api.playbattlegrounds.com/shards/pc-eu/players?filter[playerNames]=${req.params.username}`,
+      headers: {
+        Authorization: `Bearer ${process.env.PUBG_API_KEY}`,
+        Accept: 'application/vnd.api+json'
+      },
+      json: true
     })
-    .catch(next => {
-      if(next.message === '429 - undefined'){
-        return getData();
-      } else return next;
-    });
+      .then(season => {
+        const seasonData = season.data[0];
+        const matches = seasonData.relationships.matches.data;
+        seasonData.name = season.data[0].attributes.name;
+
+        matches.forEach(match => {
+          rp({
+            method: 'GET',
+            url: `https://api.playbattlegrounds.com/shards/pc-eu/matches/${match.id}`,
+            headers: {
+              Accept: 'application/vnd.api+json'
+            },
+            json: true
+          })
+            .then(match => {
+              const attrs = match.data.attributes;
+              const telemetryId = match.data.relationships.assets.data[0].id;
+              const maps = {
+                Erangel_Main: 'Erangel',
+                Desert_Main: 'Miramar',
+                Savage_Main: 'Sanhok'
+              };
+
+              match.included.forEach(asset => {
+
+                if(asset.id === telemetryId) {
+
+                  tempMatch.push({
+                    id: match.data.id,
+                    telemetryURL: asset.attributes.URL,
+                    createdAt: attrs.createdAt,
+                    duration: attrs.duration,
+                    gameMode: attrs.gameMode,
+                    // isCustomMatch: attrs.isCustomMatch,
+                    mapName: maps[attrs.mapName],
+                    // stats: attrs.stats,
+                    // tags: attrs.tags,
+                    // titleId: attrs.titleId,
+                    shardId: attrs.shardId
+                  });
+                }
+              });
+              if(tempMatch.length === matches.length){
+                seasonData.matches = tempMatch;
+                showNewSeason(seasonData);
+              }
+            })
+            .catch(next => {
+              if(!oldSeason){
+                throw 'Too many requests.';
+              } else if(next.message === '429 - undefined'){
+                showOldSeason(oldSeason);
+              } else next;
+            });
+        });
+      });
+  }
 }
 
 function matchInfo(req, res, next) {
